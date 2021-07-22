47f185ad026b383aa3df8213d85dd6a687d58b4f
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2021-07-22 11:38:57 +0200

    Create RitualAutoSpellAutopolis.js

---
+++ RitualAutoSpellAutopolis.js
@@ -0,0 +1,90 @@
+// ==UserScript==
+// @name         GREPO_RitualAutoSpell_Autopolis
+// @version      0.2
+// @description  Send spell automatically on requested polis
+// @author       Sau1707
+// @include      https://*.grepolis.com/game/*
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    const polis = [31431,32503,31409,31429,31413,31398,31406,31403,31411];
+    const dTime = [454,423,856,1062,1109,1138,1234,1309];
+    const ammount = 18;
+    const debug = true;
+    const self_polis = 32623;
+
+    var counter = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    var loop, active;
+
+    function indexOfSmallest(a) {
+        var lowest = 0;
+        for (var i = 1; i < a.length; i++) {
+            if (a[i] < a[lowest]) lowest = i;
+        }
+        return lowest;
+    }
+
+    function removeClock() {
+        for (let i = 0; i < counter.length; i++) {
+            counter[i]--;
+        }
+    }
+
+    function sendCast(id) {
+        var data = {"model_url":"CastedPowers","action_name":"cast","arguments":{"power_id":"patroness","target_id":id}};
+        if (debug) {
+            console.log("Sent to:" + id);
+            console.log(counter);
+        } else {
+            gpAjax.ajaxPost("frontend_bridge", "execute", data);
+        }
+    }
+
+// It create the red button
+setTimeout(function() {
+        var toolbar = document.getElementsByClassName("tb_activities toolbar_activities")[0];
+        var box = toolbar.getElementsByClassName("middle")[0];
+        var btdiv = document.createElement("div");
+        btdiv.className = "activity";
+        btdiv.id = "btbutton"
+        btdiv.style.filter = "brightness(70%) sepia(100%) hue-rotate(-50deg) saturate(1000%) contrast(0.8)";
+        var ptimer = document.createElement("p");
+        ptimer.id = "ptimer";
+        ptimer.style = "z-index: 6; top: -8px; float: center; position: relative; font-weight:bold";
+        //trigger button function
+        box.appendChild(btdiv);
+        btdiv.appendChild(ptimer);
+
+        btdiv.onclick = function() {
+            if (active) {
+                btdiv.style.filter = "brightness(70%) sepia(100%) hue-rotate(-50deg) saturate(1000%) contrast(0.8)";
+                active = false;
+                ptimer.innerHTML = "";
+                clearInterval(loop)
+            }
+            else {
+                btdiv.style.filter = "brightness(100%) sepia(100%) hue-rotate(90deg) saturate(1500%) contrast(0.8)";
+                active = true;
+                console.log("Active");
+                loop = setInterval(main, 1000);
+            }
+        }
+    }, 4000);
+
+
+    function main() {
+        removeClock();
+        if (ITowns.player_gods.attributes.athena_favor >= 60) {
+            var idx = indexOfSmallest(counter);
+            if (counter[idx] < 0) {
+                sendCast(polis[idx]);
+                counter[idx] = parseInt(dTime[idx]/18 + 2);
+            } else if (ITowns.player_gods.attributes.athena_favor >= 250) {
+                if (ITowns.towns[self_polis].getAvailablePopulation() > 1000) {
+                    sendCast(self_polis);
+                }
+            }
+        }
+    }
+})();
