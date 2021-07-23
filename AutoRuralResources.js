93339e269453a94a43e62c6183cf474898425ef1
Sau1707 <83017316+Sau1707@users.noreply.github.com>
2021-07-23 11:27:38 +0200

    Update AutoRuralResources.js

--- AutoRuralResources.js
+++ AutoRuralResources.js
@@ -52,8 +52,8 @@
 
     // add the button on window load
     $(document).ajaxComplete(function() {
-        let button = document.getElementById("btbutton");
-        if (button == null) {
+        let btbutton = document.getElementById("btbutton");
+        if (btbutton == null) {
            let html = '<div class=\"divider\"></div><div class="activity" id="btbutton" style="filter: brightness(70%) sepia(100%) hue-rotate(-50deg) saturate(1000%) contrast(0.8);"><p id="ptimer" style="z-index: 6; top: -8px; position: relative; font-weight: bold;"></p></div>'
            $('.tb_activities, .toolbar_activities').find(".middle").append(html);
         }
