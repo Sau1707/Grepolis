ab08001d47b42f2287323f150240d4eb71d28326
Sau1707 <Sau1707@users.noreply.github.com>
2022-11-26 16:31:40 +0000

    deploy: 07cb1631ce1f2970802069bc8e608f4218d4ce3c

---
+++ _next/static/Pyn2Z9rXr_6XtjNjSPF2J/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/b98bc7c3-7bcfdaba4de30232.js","static/chunks/575-e043703d1dadde12.js","static/chunks/pages/index-e48bc929521becf7.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
