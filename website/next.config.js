c4873a1b1ce338e6e377dd1f3c7ad528b2079a16
Sau1707 <leonardo@saurwein.ch>
2022-11-26 14:04:38 +0100

    Update settings

--- website/next.config.js
+++ website/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
 
-module.exports = nextConfig
+const env = process.env.NODE_ENV;
+
+const nextConfig = {
+	reactStrictMode: true,
+	swcMinify: true,
+	basePath: env == 'development' ? null : '/Grepolis',
+};
+
+module.exports = nextConfig;
