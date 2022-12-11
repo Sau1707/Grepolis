6ceefd639bcfae11472916dd3a869e7456ab0ba3
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2022-12-11 22:32:08 +0100

    Update and rename update1.js to update.js

---
+++ update.js
@@ -0,0 +1,19 @@
+(function () {
+  	const uw = unsafeWindow ? unsafeWindow : window;
+	if (!GM_info) return;
+    if (!GM_info.script.namespace) return;
+    if (!GM_info.script.matches) return;
+    if (!GM_info.script.matches[2]) return;
+
+	function checkForTweaksUpdate(id) {
+		if (uw.location.href != GM_info.script.matches[2]) return;
+		setTimeout(() => {
+            const evt = new CustomEvent(`gt_update_${id}`, {
+                detail: { version: GM_info.script.version },
+            });
+            uw.dispatchEvent(evt);
+        }, 100);
+	}
+
+    checkForTweaksUpdate(GM_info.script.namespace)
+})();
