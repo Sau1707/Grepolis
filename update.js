93c9438928e5740c346ad83845caf6545191bdc3
Leonardo Saurwein <83017316+Sau1707@users.noreply.github.com>
2022-12-11 22:10:30 +0100

    Update update.js

--- update.js
+++ update.js
@@ -1,11 +1,18 @@
-function checkForTweaksUpdate(id) {
+
+(function () {
+  	const uw = unsafeWindow ? unsafeWindow : window;
 	if (!GM_info) return;
-	if (window.location.href != GM_info.script.matches[2]) return;
-	setTimeout(() => {
-		const uw = unsafeWindow ? unsafeWindow : window;
+	console.log(GM_info);
+	
+	function checkForTweaksUpdate(id) {
+		if (uw.location.href != GM_info.script.matches[2]) return;
+		setTimeout(() => {
 		const evt = new CustomEvent(`gt_update_${id}`, {
 			detail: { version: GM_info.script.version },
 		});
 		uw.dispatchEvent(evt);
 	}, 100);
-}
+	}
+})();
+
+
